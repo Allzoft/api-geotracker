9826f49d9c6cea0019476553cdb0ffdae5f52e84
Zolouz966 <zrodrigo96@gmail.com>
2024-07-26 21:25:56 -0400

    we end config app module

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'API - Geotracker v0.0.1!';
   }
 }
